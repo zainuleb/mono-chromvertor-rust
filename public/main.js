de84f79ce3e52b5e5d2f9d8a713b02f22ee23d19
zainuleb <zainulebadd@gmail.com>
2022-09-19 14:18:07 +0500

    Completed filter and upload

--- public/main.js
+++ public/main.js
@@ -3,20 +3,23 @@ async function init() {
 
   try {
     rustApp = await import('../pkg');
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.error(err);
     return;
   }
+
+  console.log(rustApp);
+
   const input = document.getElementById('upload');
   const fileReader = new FileReader();
 
-  fileReader.onload = () => {
+  fileReader.onloadend = () => {
     let base64 = fileReader.result.replace(
       /^data:image\/(png|jpeg|jpg);base64,/,
       ''
     );
-
-    rustApp.grayscale(base64);
+    let img_data_url = rustApp.grayscale(base64);
+    document.getElementById('new-img').setAttribute('src', img_data_url);
   };
 
   input.addEventListener('change', () => {
--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,25 @@ use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 use base64::{ encode, decode };
 use image::load_from_memory;
+use image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
-pub fn grayscale(encoded_file: &str){
+pub fn grayscale(encoded_file: &str) -> String {
 
-    log(&"Grayscale called".into());
-    let base64_to_vector = decode(encoded_file).unwrap();
-    log(&"Image Decoded".into());
-    let img = load_from_memory(&base64_to_vector).unwrap();
-    log(&"Image Loaded".into());
+  let base64_to_vector = decode(encoded_file).unwrap();
+
+  let mut img = load_from_memory(&base64_to_vector).unwrap();
+
+  img = img.grayscale();
+
+  let mut buffer = vec![];
+  img.write_to(&mut buffer, Png).unwrap();
+
+  let encoded_img = encode(&buffer);
+  let data_url = format!(
+    "data:image/png;base64,{}",
+    encoded_img
+  );
+
+  data_url
 }
